087a1b65f8bff1f02273d335c02a8f7a1bb05fae
Marco Paiva <tulio.paiva10@gmail.com>
2023-11-27 22:36:10 -0300

    feat: Working with React router. Starting to create the first routes

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWfhkTAlxpQ4S0wXOLROFdtI4D3Y_uUKo",
+  authDomain: "bloggram-76a3f.firebaseapp.com",
+  projectId: "bloggram-76a3f",
+  storageBucket: "bloggram-76a3f.appspot.com",
+  messagingSenderId: "516354076619",
+  appId: "1:516354076619:web:df1ac8dcb065b19ac1104e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Initialize database from firebase - firestore
+const db = getFirestore(app);
+
+export {db};
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+//css
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+export default Home
